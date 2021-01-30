4ea3d355f842d9d481e15bdb34ed432cfa976703
Cesar Joel <cgurrolaescobedo@gmail.com>
2021-01-29 23:52:35 -0600

    activities Module

---
+++ activitiesmodule/src/main/java/com/cesargurrola/activitiesmodule/ActivitiesModuleActivity.kt
@@ -0,0 +1,11 @@
+package com.cesargurrola.activitiesmodule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitiesModuleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activities_module)
+    }
+}
